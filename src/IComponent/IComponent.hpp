e93e87f3f6622360800e15377387b8d71ace0784
root <arthur.doriel@epitech.eu>
2024-02-10 17:12:15 +0100

    :poop: branch test archi

---
+++ src/IComponent/IComponent.hpp
@@ -0,0 +1,27 @@
+/*
+** EPITECH PROJECT, 2024
+** B-OOP-400-REN-4-1-tekspice-arthur.doriel [WSL: Ubuntu]
+** File description:
+** IComponent
+*/
+
+#pragma once
+
+#include <string>
+
+namespace nts {
+
+    enum Tristate {
+        Undefined = (-true ),
+        True = true,
+        False = false
+    };
+
+    class IComponent {
+        public:
+            virtual ~IComponent() = default;
+            virtual void simulate(std::size_t tick) = 0;
+            virtual nts::Tristate compute(std::size_t pin) = 0;
+            virtual void setLink(std::size_t pin, nts::IComponent & other, std ::size_t otherPin) = 0;
+    };
+}
